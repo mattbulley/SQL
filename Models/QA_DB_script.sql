50245fddab8c0c27b259f9032277ac9ad969ba12
mattbulley <mattbulley01@gmail.com>
2018-02-21 11:22:24 +0000

    forward-engineer-passed

--- Models/QA_DB_script.sql
+++ Models/QA_DB_script.sql
@@ -18,9 +18,9 @@ CREATE SCHEMA IF NOT EXISTS `QAConsultingAcademy_DB` ;
 USE `QAConsultingAcademy_DB` ;
 
 -- -----------------------------------------------------
--- Table `QAConsultingAcademy_DB`.`Room`
+-- Table `QAConsultingAcademy_DB`.`Rooms`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Room` (
+CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Rooms` (
   `roomID` BIGINT(13) NOT NULL AUTO_INCREMENT,
   `vacant` TINYINT(1) NULL,
   PRIMARY KEY (`roomID`))
@@ -28,73 +28,69 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QAConsultingAcademy_DB`.`Course`
+-- Table `QAConsultingAcademy_DB`.`Courses`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Course` (
-  `courseID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Courses` (
+  `courseID` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(225) NOT NULL,
   `start` DATETIME NOT NULL,
   `end` DATETIME NOT NULL,
   `Room_roomID` BIGINT(13) NOT NULL,
-  PRIMARY KEY (`courseID`, `Room_roomID`),
-  INDEX `fk_Course_Room_idx` (`Room_roomID` ASC),
-  UNIQUE INDEX `courseID_UNIQUE` (`courseID` ASC),
-  CONSTRAINT `fk_Course_Room`
-    FOREIGN KEY (`Room_roomID`)
-    REFERENCES `QAConsultingAcademy_DB`.`Room` (`roomID`)
+  `Rooms_roomID` BIGINT(13) NOT NULL,
+  PRIMARY KEY (`courseID`, `Rooms_roomID`),
+  INDEX `fk_Courses_Rooms1_idx` (`Rooms_roomID` ASC),
+  CONSTRAINT `fk_Courses_Rooms1`
+    FOREIGN KEY (`Rooms_roomID`)
+    REFERENCES `QAConsultingAcademy_DB`.`Rooms` (`roomID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QAConsultingAcademy_DB`.`Consultant`
+-- Table `QAConsultingAcademy_DB`.`Clients`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Consultant` (
-  `consultantID` BIGINT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Clients` (
+  `clientID` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`clientID`),
+  UNIQUE INDEX `clientID_UNIQUE` (`clientID` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `QAConsultingAcademy_DB`.`Consultants`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Consultants` (
+  `consultantID` BIGINT(10) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `clientName` VARCHAR(255) NULL,
-  PRIMARY KEY (`consultantID`),
-  UNIQUE INDEX `clientName_UNIQUE` (`clientName` ASC))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `QAConsultingAcademy_DB`.`Consultant_has_Course`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Consultant_has_Course` (
-  `Consultant_consultantID` BIGINT(10) NOT NULL,
-  `Course_courseID` BIGINT(20) NOT NULL,
-  `Course_Room_roomID` BIGINT(13) NOT NULL,
-  PRIMARY KEY (`Consultant_consultantID`, `Course_courseID`, `Course_Room_roomID`),
-  INDEX `fk_Consultant_has_Course_Course1_idx` (`Course_courseID` ASC, `Course_Room_roomID` ASC),
-  INDEX `fk_Consultant_has_Course_Consultant1_idx` (`Consultant_consultantID` ASC),
-  CONSTRAINT `fk_Consultant_has_Course_Consultant1`
-    FOREIGN KEY (`Consultant_consultantID`)
-    REFERENCES `QAConsultingAcademy_DB`.`Consultant` (`consultantID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Consultant_has_Course_Course1`
-    FOREIGN KEY (`Course_courseID` , `Course_Room_roomID`)
-    REFERENCES `QAConsultingAcademy_DB`.`Course` (`courseID` , `Room_roomID`)
+  `Client_clientID` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`consultantID`, `Client_clientID`),
+  UNIQUE INDEX `clientName_UNIQUE` (`clientName` ASC),
+  INDEX `fk_Consultant_Client1_idx` (`Client_clientID` ASC),
+  UNIQUE INDEX `consultantID_UNIQUE` (`consultantID` ASC),
+  CONSTRAINT `fk_Consultant_Client1`
+    FOREIGN KEY (`Client_clientID`)
+    REFERENCES `QAConsultingAcademy_DB`.`Clients` (`clientID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QAConsultingAcademy_DB`.`Trainer`
+-- Table `QAConsultingAcademy_DB`.`Trainers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Trainer` (
-  `trainerID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Trainers` (
+  `trainerID` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL,
-  `Course_courseID` BIGINT(20) NOT NULL,
-  `Course_Room_roomID` BIGINT(13) NOT NULL,
-  PRIMARY KEY (`trainerID`, `Course_courseID`, `Course_Room_roomID`),
-  INDEX `fk_Trainer_Course1_idx` (`Course_courseID` ASC, `Course_Room_roomID` ASC),
-  CONSTRAINT `fk_Trainer_Course1`
-    FOREIGN KEY (`Course_courseID` , `Course_Room_roomID`)
-    REFERENCES `QAConsultingAcademy_DB`.`Course` (`courseID` , `Room_roomID`)
+  `Courses_courseID` BIGINT(20) NOT NULL,
+  `Courses_Rooms_roomID` BIGINT(13) NOT NULL,
+  PRIMARY KEY (`trainerID`),
+  INDEX `fk_Trainers_Courses1_idx` (`Courses_courseID` ASC, `Courses_Rooms_roomID` ASC),
+  CONSTRAINT `fk_Trainers_Courses1`
+    FOREIGN KEY (`Courses_courseID` , `Courses_Rooms_roomID`)
+    REFERENCES `QAConsultingAcademy_DB`.`Courses` (`courseID` , `Rooms_roomID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -121,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Consultant_has_CompletedCou
   INDEX `fk_Consultant_has_CompletedCourses_Consultant1_idx` (`Consultant_consultantID` ASC),
   CONSTRAINT `fk_Consultant_has_CompletedCourses_Consultant1`
     FOREIGN KEY (`Consultant_consultantID`)
-    REFERENCES `QAConsultingAcademy_DB`.`Consultant` (`consultantID`)
+    REFERENCES `QAConsultingAcademy_DB`.`Consultants` (`consultantID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Consultant_has_CompletedCourses_CompletedCourses1`
@@ -133,18 +129,24 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `QAConsultingAcademy_DB`.`Client`
+-- Table `QAConsultingAcademy_DB`.`Consultants_has_Courses`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Client` (
-  `clientID` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `Consultant_consultantID` BIGINT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`clientID`, `Consultant_consultantID`),
-  UNIQUE INDEX `clientID_UNIQUE` (`clientID` ASC),
-  INDEX `fk_Client_Consultant1_idx` (`Consultant_consultantID` ASC),
-  CONSTRAINT `fk_Client_Consultant1`
-    FOREIGN KEY (`Consultant_consultantID`)
-    REFERENCES `QAConsultingAcademy_DB`.`Consultant` (`consultantID`)
+CREATE TABLE IF NOT EXISTS `QAConsultingAcademy_DB`.`Consultants_has_Courses` (
+  `Consultants_consultantID` BIGINT(10) NOT NULL,
+  `Consultants_Client_clientID` BIGINT(20) NOT NULL,
+  `Courses_courseID` BIGINT(20) NOT NULL,
+  `Courses_Rooms_roomID` BIGINT(13) NOT NULL,
+  PRIMARY KEY (`Consultants_consultantID`, `Consultants_Client_clientID`, `Courses_courseID`, `Courses_Rooms_roomID`),
+  INDEX `fk_Consultants_has_Courses_Courses1_idx` (`Courses_courseID` ASC, `Courses_Rooms_roomID` ASC),
+  INDEX `fk_Consultants_has_Courses_Consultants1_idx` (`Consultants_consultantID` ASC, `Consultants_Client_clientID` ASC),
+  CONSTRAINT `fk_Consultants_has_Courses_Consultants1`
+    FOREIGN KEY (`Consultants_consultantID` , `Consultants_Client_clientID`)
+    REFERENCES `QAConsultingAcademy_DB`.`Consultants` (`consultantID` , `Client_clientID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Consultants_has_Courses_Courses1`
+    FOREIGN KEY (`Courses_courseID` , `Courses_Rooms_roomID`)
+    REFERENCES `QAConsultingAcademy_DB`.`Courses` (`courseID` , `Rooms_roomID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
